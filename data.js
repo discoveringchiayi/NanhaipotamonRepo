7245f774e3d19beca8d828073db01c3236850ae2
Axolotl5517 <discoveringchiayi@gmail.com>
2025-10-13 20:49:17 +0800

    add virtual tour

---
+++ data.js
@@ -0,0 +1,402 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1_landscapearchitecture",
+      "name": "1_LandscapeArchitecture",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1459375110502208,
+          "pitch": -0.05612588567991672,
+          "rotation": 1.5707963267948966,
+          "target": "1-2"
+        },
+        {
+          "yaw": 1.960899567171552,
+          "pitch": -0.0043960794066641995,
+          "rotation": 4.71238898038469,
+          "target": "4-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.6791854951558598,
+        "pitch": -0.0000227436178672491,
+        "fov": 1.299695808558111
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8070862698326131,
+          "pitch": 0.015471385903014223,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 0.7768824853287999,
+          "pitch": 0.05590724750689802,
+          "rotation": 0.7853981633974483,
+          "target": "0-1_landscapearchitecture"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.828247191615093,
+        "pitch": -0.1415236062694376,
+        "fov": 1.299695808558111
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.19468304953686832,
+          "pitch": 0.004669303291743532,
+          "rotation": 4.71238898038469,
+          "target": "1-2"
+        },
+        {
+          "yaw": -2.9756625627994087,
+          "pitch": -0.017793190175414253,
+          "rotation": 0,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.1575866518552935,
+        "pitch": -0.061748101261537514,
+        "fov": 1.299695808558111
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8414555403288375,
+          "pitch": -0.011516734053270028,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.757511981577646,
+        "pitch": 0.0458509793754569,
+        "fov": 1.299695808558111
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.005033493989517,
+          "pitch": -0.018096687263820854,
+          "rotation": 4.71238898038469,
+          "target": "5-6"
+        },
+        {
+          "yaw": -0.39693748485429126,
+          "pitch": -0.02000759609225433,
+          "rotation": 4.71238898038469,
+          "target": "8-9"
+        },
+        {
+          "yaw": 1.1822110827146997,
+          "pitch": 0.0058631450830883836,
+          "rotation": 1.5707963267948966,
+          "target": "0-1_landscapearchitecture"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-6",
+      "name": "6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.9332739890329655,
+        "pitch": -0.04578250155480035,
+        "fov": 1.299695808558111
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0727332107869785,
+          "pitch": -0.06331731033693799,
+          "rotation": 4.71238898038469,
+          "target": "6-7"
+        },
+        {
+          "yaw": 0.8730984262442512,
+          "pitch": 0.04558661197012093,
+          "rotation": 0,
+          "target": "4-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-7",
+      "name": "7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.7168281367149056,
+        "pitch": -0.033449796607914806,
+        "fov": 1.299695808558111
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0446234821018034,
+          "pitch": 0.061536319063304035,
+          "rotation": 0,
+          "target": "7-8"
+        },
+        {
+          "yaw": 1.1132240398350781,
+          "pitch": -0.05329985794144143,
+          "rotation": 1.5707963267948966,
+          "target": "5-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.12203474972762152,
+          "pitch": 0.4468343525621137,
+          "title": "南海溪蟹的家",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-8",
+      "name": "8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.3446525643615974,
+        "pitch": -0.009684154497264785,
+        "fov": 1.299695808558111
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.954178963450804,
+          "pitch": -0.009724170659861642,
+          "rotation": 0,
+          "target": "6-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-9",
+      "name": "9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.194901595560946,
+        "pitch": 0.029358275069570894,
+        "fov": 1.299695808558111
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21616018287240202,
+          "pitch": 0.07753900078088627,
+          "rotation": 12.566370614359176,
+          "target": "4-5"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Nanhaipotamon",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
